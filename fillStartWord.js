d01c78a6f24d1dc19aab6aaf7e73daff112c6a29
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-09-04 00:19:42 +0700

    HW1

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,13 +2,16 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
 
-function fillStartWord(startWord, word){
-  if(word == null){
-    return underfined
-}
-if(word.includes(startWord) == true){
-  return word
-}
+  function fillStartWord(startWord,word){
+    if(word === null || word === undefined){
+        return console.log(undefined)
+    }
+    else if(word.includes(startWord) == true){
+        return console.log(word)
+    }
+    else{
+        return console.log(startWord+word)
+    }
 }
 }
 module.exports = fillStartWord
